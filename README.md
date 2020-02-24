e81f30f15b49c27ec30854f5047442183241e4fb
Student <example@example.com>
2020-02-24 12:40:37 -0800

    remove key from old repo

---
+++ README.md
@@ -0,0 +1,37 @@
+# _Doctor Api_
+
+#### _An application to show you doctors in Portland, OR._
+
+#### By _**Mariam Alaeddin**_
+
+## Description
+
+Looking for a doctor in Portland, OR?  Well, you've come to the right page!  All you need to do is enter your primary symptom or some letters out of a specific doctor's name and we will help you find the right one! 
+
+## Setup/Installation Requirements
+
+* Install Node.js here: https://nodejs.org/en/download/
+* Go to this GitHub repository: https://github.com/MMAlaeddin/doctor-api
+* Clone the repo
+* Open your terminal/gitbash
+* cd to your desktop
+* cd into folder
+* code .
+
+## Known Bugs
+
+No known bugs
+
+## Support and contact details
+
+Mariam Alaeddin: maralaeddin@gmail.com
+
+## Technologies Used
+
+Git, HTML, CSS, JavaScript, jQuery, NPM, webpack, BetterDoctor API
+
+### License
+
+*MIT License*
+
+Copyright (c) 2020 **_{Mariam Alaeddin}_**
---
+++ src/main.js
@@ -0,0 +1,61 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+import { DoctorInfo } from './../src/project-service.js';
+import { DoctorSymp } from './../src/project-service.js';
+
+$(document).ready(function() {
+  $("#docInfo").submit(function(event) {
+    event.preventDefault();
+    const name = $("#docName").val();
+    $("#docName").val("");
+
+    (async () => {
+      let doctorInfo = new DoctorInfo();
+      const response = await doctorInfo.getInfo(name);
+      getElements(response);
+    })();
+
+    function getElements(response) {
+      const docArr = response.data;
+      const nameArr = [];
+      if (response.data.length === 0) {
+        alert("Sorry, no results for that.  Please try again.")
+      } else if (response) {
+        for (let i = 0; i < docArr.length; i++) {
+          nameArr.push(`<li> ${response.data[i].profile.first_name} ${response.data[i].profile.last_name} - ${response.data[i].practices[i].visit_address.street} - ${response.data[i].practices[i].accepts_new_patients} </li>`);
+ 
+        
+      }
+    }
+      $("#name").append(nameArr);
+    }
+  });
+});
+
+$("#sympForm").submit(function(event) {
+  event.preventDefault();
+  const symps = $("#symptoms").val();
+  $("#symptoms").val("");
+
+  (async () => {
+    let doctorSymp = new DoctorSymp();
+    const response2 = await doctorSymp.getSymp(symps);
+    getElements2(response2);
+  })();
+
+  function getElements2(response2) {
+    const docArr2 = response2.data;
+    const sympArr = [];
+    if (response2.data.length === 0) {
+      alert("Sorry, no results for that.  Please try again.")
+    } else if (response2) {
+      for (let j = 0; j < docArr2.length; j++) {
+        sympArr.push(`<li> ${response2.data[j].profile.first_name} <br> ${response2.data[j].profile.last_name} <br> ${response2.data[j].practices[0].visit_address.street} <br> ${response2.data[j].practices[0].accepts_new_patients} <br> <a href="${response2.data[j].practices[0].website}">Link to website</a> </li>`);
+      }
+    }
+    console.log(response2)
+    $("#name").append(sympArr);
+  }
+});
