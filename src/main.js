375d9de13a20e37ef0c59c75941d5f132b57f3d1
Student <example@example.com>
2020-02-24 18:48:43 -0800

    change output if link is undefined to say that there is no link available

--- src/main.js
+++ src/main.js
@@ -24,7 +24,7 @@ $(document).ready(function() {
         alert("Sorry, no results for that.  Please try again.")
       } else if (response) {
         for (let i = 0; i < docArr.length; i++) {
-          nameArr.push(`<li><strong> ${response.data[i].profile.first_name} ${response.data[i].profile.last_name}</strong> <br> ${response.data[i].practices[0].visit_address.street} <br> ${response.data[i].practices[i].accepts_new_patients? 'Is accepting new patients' : 'Is not accepting new patients'} </li>`);
+          nameArr.push(`<li><strong> ${response.data[i].profile.first_name} ${response.data[i].profile.last_name}</strong> <br> ${response.data[i].practices[0].visit_address.street} <br> ${response.data[i].practices[0].accepts_new_patients? 'Is accepting new patients' : 'Is not accepting new patients'} <br> ${response.data[i].practices[0].website? '<a href="`${response.data[i].practices[0].website}`">Link to website</a>' : "No Link"} </li>`);
       }
     }
       $("#name").append(nameArr);
@@ -50,7 +50,7 @@ $("#sympForm").submit(function(event) {
       alert("Sorry, no results for that.  Please try again.")
     } else if (response2) {
       for (let j = 0; j < docArr2.length; j++) {
-        sympArr.push(`<li><strong> ${response2.data[j].profile.first_name} ${response2.data[j].profile.last_name} </strong><br> ${response2.data[j].practices[0].visit_address.street} <br> ${response2.data[j].practices[0].accepts_new_patients? 'Is accepting new patients' : 'Is not accepting new patients'} <br> <a href="${response2.data[j].practices[0].website}">Link to website</a> </li>`);
+        sympArr.push(`<li><strong> ${response2.data[j].profile.first_name} ${response2.data[j].profile.last_name} </strong><br> ${response2.data[j].practices[0].visit_address.street} <br> ${response2.data[j].practices[0].accepts_new_patients? 'Is accepting new patients' : 'Is not accepting new patients'} <br> ${response2.data[j].practices[0].website? '<a href="`${response.data[i].practices[0].website}`">Link to website</a>' : "No Website"} </li>`);
       }
     }
     $("#name").append(sympArr);
