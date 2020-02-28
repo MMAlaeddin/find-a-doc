2442b5fe65df71b7321bf698f43daeff8294e0b7
Mariam Alaeddin <mariamalaeddin@gmail.com>
2020-02-27 16:29:56 -0800

    update linter and spacing as well as moving sympform into doc ready

--- src/main.js
+++ src/main.js
@@ -21,38 +21,37 @@ $(document).ready(function() {
       const docArr = response.data;
       const nameArr = [];
       if (response.data.length === 0) {
-        alert("Sorry, no results for that.  Please try again.")
+        alert("Sorry, no results for that.  Please try again.");
       } else if (response) {
         for (let i = 0; i < docArr.length; i++) {
           nameArr.push(`<li><strong> ${response.data[i].profile.first_name} ${response.data[i].profile.last_name}</strong> <br> ${response.data[i].practices[0].visit_address.street} <br> ${response.data[i].practices[0].phones[0].number} <br>${response.data[i].practices[0].accepts_new_patients? 'Is accepting new patients' : 'Is not accepting new patients'} <br> ${response.data[i].practices[0].website? '<a href="`${response.data[i].practices[0].website}`">Link to website</a>' : "No Link"} </li>`);
+        }
       }
-    }
       $("#name").append(nameArr);
     }
   });
-});
-
-$("#sympForm").submit(function(event) {
-  event.preventDefault();
-  const symps = $("#symptoms").val();
-  $("#symptoms").val("");
-
-  (async () => {
-    let doctorSymp = new DoctorSymp();
-    const response2 = await doctorSymp.getSymp(symps);
-    getElements2(response2);
-  })();
-
-  function getElements2(response2) {
-    const docArr2 = response2.data;
-    const sympArr = [];
-    if (response2.data.length === 0) {
-      alert("Sorry, no results for that.  Please try again.")
-    } else if (response2) {
-      for (let j = 0; j < docArr2.length; j++) {
-        sympArr.push(`<li><strong> ${response2.data[j].profile.first_name} ${response2.data[j].profile.last_name} </strong><br> ${response2.data[j].practices[0].visit_address.street} <br> ${response2.data[j].practices[0].phones[0].number} <br> ${response2.data[j].practices[0].accepts_new_patients? 'Is accepting new patients' : 'Is not accepting new patients'} <br> ${response2.data[j].practices[0].website? '<a href="`${response.data[i].practices[0].website}`">Link to website</a>' : "No Website"} </li>`);
+  $("#sympForm").submit(function(event) {
+    event.preventDefault();
+    const symps = $("#symptoms").val();
+    $("#symptoms").val("");
+  
+    (async () => {
+      let doctorSymp = new DoctorSymp();
+      const response2 = await doctorSymp.getSymp(symps);
+      getElements2(response2);
+    })();
+  
+    function getElements2(response2) {
+      const docArr2 = response2.data;
+      const sympArr = [];
+      if (response2.data.length === 0) {
+        alert("Sorry, no results for that.  Please try again.");
+      } else if (response2) {
+        for (let j = 0; j < docArr2.length; j++) {
+          sympArr.push(`<li><strong> ${response2.data[j].profile.first_name} ${response2.data[j].profile.last_name} </strong><br> ${response2.data[j].practices[0].visit_address.street} <br> ${response2.data[j].practices[0].phones[0].number} <br> ${response2.data[j].practices[0].accepts_new_patients? 'Is accepting new patients' : 'Is not accepting new patients'} <br> ${response2.data[j].practices[0].website? '<a href="`${response.data[i].practices[0].website}`">Link to website</a>' : "No Website"} </li>`);
+        }
       }
+      $("#name").append(sympArr);
     }
-    $("#name").append(sympArr);
-  }
+  });
 });
--- src/project-service.js
+++ src/project-service.js
@@ -1,32 +1,32 @@
 export class DoctorInfo {
-	async getInfo(name) {
-			try {
-				let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`);
-				let jsonifiedResponse;
-				if (response.ok && response.status == 200) {
-					jsonifiedResponse = await response.json();
-				} else {
-					jsonifiedResponse = false; 
-				}
-				return jsonifiedResponse;
-			 	} catch(error) {
-					 return false;
-				 }
-			}
+  async getInfo(name) {
+    try {
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`);
+      let jsonifiedResponse;
+      if (response.ok && response.status == 200) {
+        jsonifiedResponse = await response.json();
+      } else {
+        jsonifiedResponse = false; 
+      }
+      return jsonifiedResponse;
+    } catch(error) {
+      return false;
     }
-    export class DoctorSymp {
-      async getSymp(symps) {
-          try {
-            let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${symps}&location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`);
-            let jsonifiedResponse;
-            if (response.ok && response.status == 200) {
-              jsonifiedResponse = await response.json();
-            } else {
-              jsonifiedResponse = false; 
-            }
-            return jsonifiedResponse;
-             } catch(error) {
-               return false;
-             }
-          }
-        }
+  }
+}
+export class DoctorSymp {
+  async getSymp(symps) {
+    try {
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${symps}&location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`);
+      let jsonifiedResponse;
+      if (response.ok && response.status == 200) {
+        jsonifiedResponse = await response.json();
+      } else {
+        jsonifiedResponse = false; 
+      }
+      return jsonifiedResponse;
+    } catch(error) {
+      return false;
+    }
+  }
+}
